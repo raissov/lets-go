a25844c7bb4e34e2ce79d8509e45c20d55c59e7f
raissov <brown.robert@bk.ru>
2021-07-15 14:20:30 +0600

    making small functions

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -2,20 +2,16 @@ package main
 
 import (
 	"fmt"
-	"github.com/julienschmidt/httprouter"
 	"net/http"
-	"strconv"
 )
 
-func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request)  {
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "create a new moview")
 }
 
-func (app *application) showMovieHandler (w http.ResponseWriter, r *http.Request) {
-	params := httprouter.ParamsFromContext(r.Context())
-
-	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
-	if err != nil || id < 1 {
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil {
 		http.NotFound(w, r)
 		return
 	}
